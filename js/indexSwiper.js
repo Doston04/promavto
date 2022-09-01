04d0df48c04f5d628d0ab9dd31a0e35c0b572f85
Doston04 <avazovjr@gmail.com>
2022-09-01 17:37:05 +0500

    home page some sections are added

---
+++ js/indexSwiper.js
@@ -0,0 +1,54 @@
+var developmentsSwiper = new Swiper(".developmentsSwiper", {
+    breakpoints: {
+        600: {
+            slidesPerView: 1.8,
+            spaceBetween: 14,
+        },
+        768: {
+            slidesPerView: 2.6,
+            spaceBetween: 24,
+        },
+    },
+    slidesPerView: 1,
+    spaceBetween: 16,
+    autoplay: {
+        delay: 1800,
+        disableOnInteraction: false,
+    },
+    speed: 2000,
+    loop: true,
+})
+
+
+var partnersSwiper = new Swiper(".parntersSwiper", {
+    breakpoints: {
+        600: {
+            slidesPerView: 2.3,
+            spaceBetween: 14,
+        },
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 24,
+        },
+        1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+    },
+    slidesPerView: 1,
+    spaceBetween: 8,
+    loop: true,
+    autoplay: {
+        delay: 1800,
+        disableOnInteraction: false,
+    },
+    speed: 1200,
+    navigation: {
+        nextEl: ".nextBtn",
+        prevEl: ".prevBtn",
+    },
+}); 
