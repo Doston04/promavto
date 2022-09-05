3c806ac38a9db5e364acc18e7f0c88bac450cf16
Doston04 <avazovjr@gmail.com>
2022-09-05 17:20:56 +0500

    modal added

---
+++ js/isothermics-swiper.js
@@ -0,0 +1,55 @@
+var swiper = new Swiper(".isothermicMini1", {
+  spaceBetween: 20,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".isothermicBig1", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+var swiper = new Swiper(".isothermicMini2", {
+  spaceBetween: 20,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".isothermicBig2", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+var swiper = new Swiper(".isothermicMini3", {
+  spaceBetween: 20,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".isothermicBig3", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+var swiper = new Swiper(".isothermicMini4", {
+  spaceBetween: 20,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".isothermicBig4", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  thumbs: {
+    swiper: swiper,
+  },
+});
--- js/isothermics.js
+++ js/isothermics.js
@@ -15,42 +15,23 @@ buttons.forEach((btn, i) => {
     })
 })
 
+const btns = document.querySelectorAll(".isothermic__link")
+const modal = document.querySelector(".modal")
+const modalContent = document.querySelector(".modal__inner")
+const xBtn = document.querySelector(".xBtn")
 
-// const miniImages = document.querySelectorAll(".miniImage")
-// const images = document.querySelectorAll(".imageImage")
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        modal.style.display = "flex"
+    })
+})
 
-// miniImages.forEach((miniImage, index) => {
-//     miniImage.addEventListener("click", () => {
-//         images.forEach(image => {
-//             image.classList.remove("active")
-//         });
-
-//         images[index].classList.add("active");
-//     });
-// });
-
-// const miniImages2 = document.querySelectorAll(".miniImage2")
-// const images2 = document.querySelectorAll(".imageImage2")
-
-// miniImages2.forEach((miniImage, index) => {
-//     miniImage.addEventListener("click", () => {
-//         images2.forEach(image => {
-//             image.classList.remove("active")
-//         });
-
-//         images2[index].classList.add("active");
-//     });
-// });
-
-
-var swiper = new Swiper(".swiper1Thumbs", {
-    loop: true,
-    freeMode: true,
-    watchSlidesProgress: true,
-});
-new Swiper(".swiper1", {
-    loop: true,
-    thumbs: {
-        swiper: swiper,
-    },
-});
+window.addEventListener("click", (e) => {
+    if (e.target == modal) {
+        modal.style.display = "none"
+    } else if (e.target == xBtn) {
+        modal.style.display = "none"
+    } else if (e.target == modalContent) {
+        modal.style.display = "flex"
+    }
+})
